29dba56a72330473e4487934d8a9d6a6150d1411
Ghalib-Butt <Ghalibnbutt@gmail.com>
2023-03-30 20:40:59 +0500

    Fixed prime number method and added location method in basic.py

--- basic.py
+++ basic.py
@@ -3,6 +3,9 @@ from os import system
 from requests import get, codes
 from keyboard import read_key
 from json import loads
+from geopy.geocoders import Nominatim
+import geocoder
+
 
 # Method for option 1
 def getUserDetails():
@@ -31,14 +34,22 @@ def checkPrimeNumber():
     if number < 2:
         isPrime = False
     elif number > 4:
-        for num in range(2, round(number/3)):
+        for num in range(2, round(number/3)+1):
             if (number % num == 0):
-                isPrime = True
+                isPrime = False
                 break
     if isPrime:
         print(number, "is a Prime Number.")
     else:
         print(number, "is not a Prime Number.")
+
+# Method for option 4
+def printMyLocation():
+    geolocator = Nominatim(user_agent="my_app")
+    g = geocoder.ip('me')
+    location = f"{g.lat}, {g.lng}"
+    address = geolocator.reverse(location)
+    print(f"Your location is: {address.address}")
 
 def printCountriesNames(response):
     jsonResponse = loads(response)
@@ -50,6 +61,7 @@ def printMethodDetails():
     print("1. Get user details.")
     print("2. Get all countries details.")
     print("3. Check if a number is prime.")
+    print("4. Get your current location.")
 
 def selectAndExecuteMethod():
     functionNumber = input("Enter the function number: ")
@@ -59,16 +71,23 @@ def selectAndExecuteMethod():
         getAllCountriesDetails()
     if functionNumber == "3":
         checkPrimeNumber()
+    if functionNumber == "4":
+        printMyLocation()
+    else:
+        close(True)
+        selectAndExecuteMethod()
 
-def close():
-    sleep(1)
-    print("Thanks for using our services. Press any key to exit.")
+
+def close(instant):
+    if not(instant):
+        sleep(1)
+        print("Thanks for using our services. Press any key to exit.")
     if read_key():
         system('clear')
 
 def main():
     printMethodDetails()
     selectAndExecuteMethod()
-    close()
+    close(False)
     
 main()
